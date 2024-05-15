bf3f887c0f7feec525c9270eece26cdc4f53e0c2
danielGomez <gomezjesushd@gmail.com>
2024-05-14 23:04:01 -0300

    feat: Se importo la configuracion a la base de datos

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-// Importacion de las dependencias.
-import express from "express"
+//                   IMPORTACIONES  
+//              (Framework y dependencias)
+
+import express from "express";
 
 
-// importacion de las configuraciones (base de datos y el puerto).
-import { enviroments } from "./config/enviroments.js"
-
-const app = express()
+//                   IMPORTACIONES  
+//           (Base de datos y configuraciones)
 
 
+// Importación de las configuraciones (base de datos y puerto) desde el archivo de entornos.
+import { enviroments } from "./config/enviroments.js";
+import { connection } from "./config/database.js";
+
+// Creación de una instancia de la aplicación Express.
+const app = express();
+
+// Configuración del servidor para escuchar en el puerto definido en el archivo de entornos.
 app.listen(enviroments.PORT, () => {
-    console.log(`Servidor corriendo en el http://localhost:${enviroments.PORT}`)
-})
-
+    console.log(`Servidor corriendo en el http://localhost:${enviroments.PORT}`);
+});
